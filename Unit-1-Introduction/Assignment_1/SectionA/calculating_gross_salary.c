90a9faeb2205181f93b41d1567219599620d1f30
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-03 20:07:04 +0530

    Create calculating_gross_salary.c

---
+++ Unit-1-Introduction/Assignment_1/SectionA/calculating_gross_salary.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* User-defined function to calculate gross salary */
+float calculateGross(float basic)
+{
+    float da, hra, gross;
+
+    da = 0.40 * basic;
+    hra = 0.20 * basic;
+    gross = basic + da + hra;
+
+    return gross;
+}
+
+int main()
+{
+    float basic, gross;
+
+    printf("Enter basic salary: ");
+    scanf("%f", &basic);
+
+    gross = calculateGross(basic);
+
+    printf("Gross Salary = %.2f", gross);
+
+    return 0;
+}
