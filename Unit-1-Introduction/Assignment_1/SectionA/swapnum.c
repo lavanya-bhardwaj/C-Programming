e2b4be063139937efa53fab6a597466a6a6b04de
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-04 08:17:35 +0530

    Create swapnum.c

---
+++ Unit-1-Introduction/Assignment_1/SectionA/swapnum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void swap(int *x, int *y)
+{
+    int temp;
+
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main()
+{
+    int C, D;
+
+    printf("Enter value of C: ");
+    scanf("%d", &C);
+
+    printf("Enter value of D: ");
+    scanf("%d", &D);
+
+    swap(&C, &D);
+
+    printf("After swapping:\n");
+    printf("C = %d\n", C);
+    printf("D = %d\n", D);
+
+    return 0;
+}
