624c88038e345e0d2558a9d487a675a44cea2eaa
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-10 01:53:33 +0530

    Create time_conversion.c

---
+++ Unit-1-Introduction/Assignment_1/SectionA/time_conversion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void convertTime(int seconds) {
+    int hrs, mins, secs;
+    hrs = seconds / 3600;
+    seconds = seconds % 3600;
+    mins = seconds / 60;
+    secs = seconds % 60;
+    printf("Time = %d:%d:%d\n", hrs, mins, secs);
+}
+
+int main() {
+    int seconds;
+    printf("Enter total seconds: ");
+    scanf("%d", &seconds);
+    convertTime(seconds);
+    return 0;
+}
