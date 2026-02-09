70ffb1bf27c2eb4f67d4b190770f41bbd116791b
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-10 02:03:15 +0530

    Create odd_or_even.c

---
+++ Unit-1-Introduction/Assignment_1/SectionB/odd_or_even.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void checkEvenOdd(int num) {
+    if (num % 2 == 0)
+        printf("%d is an Even number\n", num);
+    else
+        printf("%d is an Odd number\n", num);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    checkEvenOdd(num);
+    return 0;
+}
