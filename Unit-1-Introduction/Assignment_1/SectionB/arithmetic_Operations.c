44a8aeb799547d7cca3150011a0818917b29cb8b
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-10 02:05:20 +0530

    Create arithmetic_Operations.c

---
+++ Unit-1-Introduction/Assignment_1/SectionB/arithmetic_Operations.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void arithmeticOperation(int a, int b) {
+    if (a == 0 || b == 0) {
+        printf("Wrong Entry\n");
+    }
+    else if (a > b) {
+        printf("Multiplication = %d\n", a * b);
+    }
+    else if (b > a) {
+        printf("Addition = %d\n", a + b);
+    }
+    else {
+        printf("Subtraction = %d\n", a - b);
+    }
+}
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    arithmeticOperation(a, b);
+    return 0;
+}
