98cc0bc26aeaaa3e839afa760d7c722a08cb3a97
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-10 02:59:33 +0530

    Create pattern7.c

---
+++ Unit-1-Introduction/Assignment_1/SectionC/pattern7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void pattern(int n) {
+    int i, j;
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n - i; j++)
+            printf(" ");
+        for (j = 1; j <= 2*i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+    for (i = n - 1; i >= 1; i--) {
+        for (j = 1; j <= n - i; j++)
+            printf(" ");
+        for (j = 1; j <= 2*i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter number of lines: ");
+    scanf("%d", &n);
+    pattern(n);
+    return 0;
+}
