6384593d5cb3727488fa52496a24556e59d9c846
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-10 02:16:52 +0530

    Create calculate_expense.c

---
+++ Unit-1-Introduction/Assignment_1/SectionB/calculate_expense.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void calculateExpense(int qty, float price) {
+    float total, discount, finalAmount;
+    total = qty * price;
+    discount = total * 0.10;          // 10% discount
+    finalAmount = total - discount;
+    if (qty > 100 || finalAmount > 5000) {
+        finalAmount = finalAmount - (finalAmount * 0.10);
+    }
+    printf("Total amount to be paid = %.2f\n", finalAmount);
+}
+
+int main() {
+    int quantity;
+    float price;
+    printf("Enter quantity and price per item: ");
+    scanf("%d %f", &quantity, &price);
+    calculateExpense(quantity, price);
+    return 0;
+}
