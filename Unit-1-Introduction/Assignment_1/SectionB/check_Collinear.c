2f170e7e1dde9b66240cf84db9772b8e3837577d
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-10 02:21:14 +0530

    Update check_Collinear.c

--- Unit-1-Introduction/Assignment_1/SectionB/check_Collinear.c
+++ Unit-1-Introduction/Assignment_1/SectionB/check_Collinear.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 void checkCollinear(int x1, int y1, int x2, int y2, int x3, int y3) {
-    if ((x2 - x1)*(y3 - y1) == (x3 - x1)*(y2 - y1))
+    if ((x2 - x1)*(y3 - y1) == (x3 - x1)*(y2 - y1)){
         printf("All three points lie on the same straight line\n");
-    else
+    }
+    else{
         printf("Points do not lie on the same straight line\n");
+    }
 }
 int main() {
     int x1, y1, x2, y2, x3, y3;
