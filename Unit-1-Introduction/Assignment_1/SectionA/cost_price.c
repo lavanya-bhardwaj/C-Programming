d6521f28393af3797f0a01dcb805a43fcfda7797
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-10 01:39:39 +0530

    Create cost_price.c

---
+++ Unit-1-Introduction/Assignment_1/SectionA/cost_price.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+float calculateCostPrice(float sellingPrice, float profit)
+{
+    float totalCostPrice, costPerItem;
+    totalCostPrice = sellingPrice - profit;
+    costPerItem = totalCostPrice / 15;
+    return costPerItem;
+}
+
+int main()
+{
+    float sellingPrice, profit, costPrice;
+    printf("Enter total selling price of 15 items: ");
+    scanf("%f", &sellingPrice);
+    printf("Enter total profit earned: ");
+    scanf("%f", &profit);
+    costPrice = calculateCostPrice(sellingPrice, profit);
+    printf("Cost Price of one item = %.2f", costPrice);
+    return 0;
+}
