34a437a6adc299dd1a3e21b6edea7642bce62db6
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-10 02:02:20 +0530

    Create finding_max.c

---
+++ Unit-1-Introduction/Assignment_1/SectionB/finding_max.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void findMax(int a, int b) {
+    if (a > b)
+        printf("Maximum number is %d\n", a);
+    else
+        printf("Maximum number is %d\n", b);
+}
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    findMax(a, b);
+    return 0;
+}
