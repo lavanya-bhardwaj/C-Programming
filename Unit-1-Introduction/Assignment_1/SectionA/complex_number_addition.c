85a5507bc79ee3b26cf35004adb1fb1cdd21cf1b
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-10 01:57:36 +0530

    Create complex_number_addition.c

---
+++ Unit-1-Introduction/Assignment_1/SectionA/complex_number_addition.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void addComplex() {
+    float r1, i1, r2, i2;
+    float rSum, iSum;
+    printf("Enter real and imaginary part of first complex number: ");
+    scanf("%f %f", &r1, &i1);
+    printf("Enter real and imaginary part of second complex number: ");
+    scanf("%f %f", &r2, &i2);
+    rSum = r1 + r2;
+    iSum = i1 + i2;
+    printf("Sum = %.2f + %.2fi\n", rSum, iSum);
+}
+
+int main() {
+    addComplex();
+    return 0;
+}
