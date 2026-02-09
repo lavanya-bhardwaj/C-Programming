daecf6d514e149da91ea63afb3d089d4e137c923
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-10 01:35:38 +0530

    Create sum_of_digits.c

---
+++ Unit-1-Introduction/Assignment_1/SectionA/sum_of_digits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int sumOfDigits(int num)
+{
+    int sum = 0, digit, i;
+    for(i = 0; i < 5; i++)
+    {
+        digit = num % 10;
+        sum = sum + digit;
+        num = num / 10;
+    }
+    return sum;
+}
+int main()
+{
+    int num, result;
+    printf("Enter a five-digit number: ");
+    scanf("%d", &num);
+    result = sumOfDigits(num);
+    printf("Sum of digits = %d", result);
+    return 0;
+}
