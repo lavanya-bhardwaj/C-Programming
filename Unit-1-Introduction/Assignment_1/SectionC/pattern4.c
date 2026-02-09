d29f5c1f66aea76be192e365085a845f0f6b705f
Lavanya Bhardwaj <lavanyabhardwaj1106@gmail.com>
2026-02-10 02:52:31 +0530

    Create pattern4.c

---
+++ Unit-1-Introduction/Assignment_1/SectionC/pattern4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void pattern4(int n) {
+    int i, j, space = 0;
+    for (i = n; i >= 1; i--) {
+        for (j = 0; j < space; j++){
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++){
+            printf("*");
+        }
+        space++;
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter number of lines: ");
+    scanf("%d", &n);
+    pattern4(n);
+    return 0;
+}
